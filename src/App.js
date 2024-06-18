fb837b1bdf8a637f4fddfa37738794b131ab8553
I500512 <y.maas@student.fontys.nl>
2024-06-18 16:28:13 +0200

    Added Context & Hooks and modified this in the App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialGlobalState = {
+  count: 0,
+};
+
+// Create a Context for the (global) State
+const GlobalState = React.createContext();
+
+class Global extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Set the initial (global) State
+    this.state = {
+      globals: initialGlobalState || {},
+    };
+  }
+
+  // Expose the setGlobals function to the Globals object
+  componentDidMount() {
+    GlobalState.set = this.setGlobalState;
+  }
+
+  setGlobalState = (data = {}) => {
+    const { globals } = this.state;
+    // Loop over the data items by key, only updating those which have changed
+    Object.keys(data).forEach((key) => {
+      globals[key] = data[key];
+  });
+
+  // Update the state with the new State
+  this.setState(globals);
+  };
+
+  render() {
+    const { globals } = this.state;
+    const { Root } = this.props;
+
+    return (
+    // Pass the current value of GlobalState, based on this components' State, down
+      <GlobalState.Provider value={globals}>
+        <Root />
+      </GlobalState.Provider>
+    );
+  }
 }
 
-export default App;
+// Create a shorthand Hook for using the GlobalState
+const useGlobalState = () => React.useContext(GlobalState);
+
+// Create an example component which both renders and modifies the GlobalState
+function SomeComponent() {
+  const { count } = useGlobalState();
+
+  // Create a function which mutates GlobalState
+  function incrementCount() {
+    GlobalState.set({
+      count: count + 1,
+    });
+  }
+  return <div onClick={incrementCount}>{count}</div>;
+}
+
+export default function App() {
+  // Note: within the Root function we can return any Component (not just SomeComponent, but also a Router for instance)
+  return <Global Root={() => <SomeComponent />} />;
+}
+
+// Expose the GlobalState object to the window (allowing GlobalState.set({ count: 'new' }) from anywhere in the code (even your console))
+window.GlobalState = GlobalState;
